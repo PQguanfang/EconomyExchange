d104dd0bf2dee7ff51b26a2737d336602ff42102
wuming <1686965528@qq.com>
2023-02-03 21:28:08 +0800

    remove unused code 1.5.1

--- src/main/java/cn/superiormc/commands/MainExchange.java
+++ src/main/java/cn/superiormc/commands/MainExchange.java
@@ -3,7 +3,6 @@ package cn.superiormc.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import cn.superiormc.configs.Messages;
 import org.jetbrains.annotations.NotNull;
 
